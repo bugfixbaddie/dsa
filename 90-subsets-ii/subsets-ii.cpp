dd2c40afa1fafa8981756b89213d876119a70b6e
vaishnavi shukla <vaishnavishukla439@gmail.com>
2026-03-29 19:21:30 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (64.86%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+   void solve(vector<int>&nums,vector<vector<int>>&ans,vector<int>&curr,int index){
+    ans.push_back(curr);
+    for(int i=index;i<nums.size();i++){
+        if(i>index && nums[i]==nums[i-1]) continue;
+        curr.push_back(nums[i]);
+        solve(nums,ans,curr,i+1);
+        curr.pop_back();
+    }
+   }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
+        vector<int>curr;
+        solve(nums,ans,curr,0);
+        return ans;
+    }
+};
