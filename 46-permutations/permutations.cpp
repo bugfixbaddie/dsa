6b72b4552ff149f3bab116c716355812849e0c3d
vaishnavi shukla <vaishnavishukla439@gmail.com>
2026-03-30 19:23:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (47.46%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+void permut(vector<int>&arr,vector<vector<int>>&ans,vector<int>&temp,vector<bool>&visited){
+    if(visited.size()==temp.size()){
+        ans.push_back(temp);
+        return;
+    }
+    for(int i=0;i<visited.size();i++){
+      if(visited[i]==0){
+        visited[i]=1;
+        temp.push_back(arr[i]);
+          permut(arr,ans,temp,visited);
+        visited[i]=0;
+        temp.pop_back();
+      }  
+  } }
+    vector<vector<int>> permute(vector<int>& arr) {
+        vector<vector<int>>ans;
+    vector<int>temp;
+    vector<bool>visited(arr.size(),0);
+    permut(arr,ans,temp,visited);
+    return ans;
+    }
+};
